6078f828dc6ba52e50617480b3a28c0d45489ec1
mohamedodooimp <mohamed.odooimp@gmail.com>
2024-10-25 17:26:17 +0300

    addons
    Co-Authored-By: mohamedodooimp <186291958+mohamedodooimp@users.noreply.github.com>

---
+++ muk_web_chatter/static/src/core/thread/thread.js
@@ -0,0 +1,28 @@
+import { patch } from "@web/core/utils/patch";
+
+import { Thread } from '@mail/core/common/thread';
+
+patch(Thread.prototype, {
+    get displayMessages() {
+        let messages = (
+            this.props.order === 'asc' ?
+            this.props.thread.nonEmptyMessages :
+            [...this.props.thread.nonEmptyMessages].reverse()
+        );
+        if (!this.props.showTrackingMessages) {
+            messages = messages.filter(
+                (msg) => msg.trackingValues.length == 0
+            );
+        }
+        return messages;
+    },
+});
+
+Thread.props = [
+    ...Thread.props,
+    'showTrackingMessages?',
+];
+Thread.defaultProps = {
+    ...Thread.defaultProps,
+    showTrackingMessages: true,
+};
